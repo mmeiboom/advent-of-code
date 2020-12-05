6c3197da3dcfe49b5b7c81c2369d11fc9113039b
Mattijs Meiboom <mmeiboom@bol.com>
2020-12-05 12:12:01 +0100

    Drop bin2dec for radix

--- src/main/kotlin/y2020/Day5.kt
+++ src/main/kotlin/y2020/Day5.kt
@@ -7,13 +7,12 @@ object Day5 : Day {
 
     private val input = resourceLines(2020, 5)
 
-    override fun part1() = input.map { seatId(it) }.maxOf{ it }
+    override fun part1() = input.map { seatId(it) }.maxOf { it }
 
-    override fun part2() : Long {
+    override fun part2(): Long {
         val sortedSeats = input.map { seatId(it) }.sorted()
         var prev = 0L
-        for (seatId in sortedSeats)
-        {
+        for (seatId in sortedSeats) {
             if (seatId - prev == 2L) {
                 return seatId - 1
             }
@@ -23,27 +22,12 @@ object Day5 : Day {
         return -1
     }
 
-    private fun seatId(s: String) = binToDec(s
+    private fun seatId(s: String) = s
             .replace('F', '0')
             .replace('B', '1')
             .replace('L', '0')
             .replace('R', '1')
-            .toLong())
-}
-
-fun binToDec(num: Long): Long {
-    var num = num
-    var decimalNumber = 0L
-    var i = 0
-    var remainder: Long
-
-    while (num.toInt() != 0) {
-        remainder = num % 10
-        num /= 10
-        decimalNumber += (remainder * Math.pow(2.0, i.toDouble())).toInt()
-        ++i
-    }
-    return decimalNumber
+            .toLong(2)
 }
 
 fun main() {
