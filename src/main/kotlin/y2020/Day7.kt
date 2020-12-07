7aa769b8ce37b9d2c5047fb8cbbfc17f68f39e81
Mattijs Meiboom <mmeiboom@bol.com>
2020-12-07 08:07:59 +0100

    Day7 QAD

--- src/main/kotlin/y2020/Day7.kt
+++ src/main/kotlin/y2020/Day7.kt
@@ -5,14 +5,44 @@ import lib.resourceLines
 
 object Day7 : Day {
 
-    private val input = resourceLines(2020, 6)
+    private val input = resourceLines(2020, 7)
 
-    override fun part1() : Long {
-        return -1
+    override fun part1(): Long {
+        val map = bagsTree()
+        return map.keys
+                .filter { canContainGoldBag(it, map) }
+                .size
+                .toLong()
     }
 
-    override fun part2() : Long {
-        return -1
+    private fun canContainGoldBag(key: String, map: Map<String, Map<String, Int>>) : Boolean {
+        val contentsForKey = map[key]!!
+        if(contentsForKey.containsKey("shiny gold")) {
+            return true
+        }
+        return contentsForKey.keys.any { canContainGoldBag(it, map) }
+    }
+
+    private fun bagsTree(): Map<String, Map<String, Int>> {
+        val map = input.map {
+            val (bag, rest) = it.split(" bags contain ")
+            val bags = """(\d+) (\w+ \w+) bags?""".toRegex().findAll(rest)
+                    .map { mr -> mr.groupValues[2] to mr.groupValues[1].toInt() }
+                    .toMap()
+
+            bag to bags
+        }.toMap()
+        return map
+    }
+
+    override fun part2(): Long {
+        val map = bagsTree()
+        return countBagsIn("shiny gold", map)
+    }
+
+    private fun countBagsIn(key: String, map: Map<String, Map<String, Int>>): Long {
+        val contentsForKey = map[key]!!
+        return contentsForKey.map { it.value + it.value * countBagsIn(it.key, map) }.sum()
     }
 }
 
