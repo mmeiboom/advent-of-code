14e468bb76a15abc9eef54b68515608b88bdacd6
Mattijs Meiboom <mmeiboom@bol.com>
2021-12-05 21:05:42 +0100

    Day 5

---
+++ src/main/kotlin/lib/Line.kt
@@ -0,0 +1,33 @@
+package lib
+
+data class Line(
+        val x1: Int,
+        val y1: Int,
+        val x2: Int,
+        val y2: Int
+    ) {
+        fun isHorizontal() = y1 == y2
+        fun isVertical() = x1 == x2
+
+        fun pointsOnLine() : List<Point> {
+            return if(isHorizontal()) {
+                range(x1,x2).map { Point(it, y1)}
+            } else if(isVertical()) {
+                range(y1,y2).map { Point(x1, it)}
+            } else {
+                val xs = range(x1, x2)
+                val ys = range(y1, y2)
+                return xs.zip(ys).map {
+                    (x,y) -> Point(x,y)
+                }
+            }
+        }
+    }
+
+    fun range(from: Int, to:Int) : IntProgression {
+        return if (to > from) {
+            IntRange(from, to)
+        } else {
+            IntProgression.fromClosedRange(from, to, -1)
+        }
+    }
--- src/main/kotlin/y2021/Day3.kt
+++ src/main/kotlin/y2021/Day3.kt
@@ -6,19 +6,53 @@ import lib.resourceLines
 
 object Day3 : Day {
 
-    private val input = resourceLines(2021, 6)
+    private val input = resourceLines(2021, 3)
 
     override fun part1() : Long {
-        return -1
+        val gammaString = IntRange(0, input[0].length - 1).map { pos ->
+            val zeroCount = input.count { it[pos] == '0' }
+            val oneCount = input.count { it[pos] == '1' }
+            if(zeroCount > oneCount) '0' else '1'
+        }.joinToString(separator = "")
+        val gamma = Integer.parseInt(gammaString, 2).toLong()
+        val epsilonString = gammaString.map { if(it == '0') '1' else '0'  }.joinToString(separator = "")
+        val epsilon = Integer.parseInt(epsilonString,2).toLong()
+        return gamma * epsilon
     }
 
     override fun part2() : Long {
-        return -1
+        val oxy = valOxy()
+        val co2 = valCo2()
+        return oxy * co2
+    }
+
+    private fun valOxy(): Long {
+        val values = input.toMutableList()
+        var pos = 0
+        while (values.size > 1 && pos < input[0].length) {
+            val oneCount = values.count { it[pos] == '1' }
+            val keepChar = if (oneCount >= values.size.toDouble() / 2) '1' else '0'
+            values.removeIf { it[pos] != keepChar }
+            pos++;
+        }
+        return Integer.parseInt(values.first(), 2).toLong()
+    }
+
+    private fun valCo2(): Int {
+        val values = input.toMutableList()
+        var pos = 0
+        while (values.size > 1 && pos < input[0].length) {
+            val zeroCount = values.count { it[pos] == '0' }
+            val keepChar = if (zeroCount <= values.size.toDouble() / 2) '0' else '1'
+            values.removeIf { it[pos] != keepChar }
+            pos++;
+        }
+        return Integer.parseInt(values.first(), 2)
     }
 
 }
 
 fun main() {
-    println(Day3.part1())
+//    println(Day3.part1())
     println(Day3.part2())
 }
