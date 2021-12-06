3ef5a38c73f8d3e7ba2fcab7481099eea5d194b2
Mattijs Meiboom <mmeiboom@bol.com>
2021-12-06 10:03:28 +0100

    Extract simulation method, cleanup

--- src/main/kotlin/y2021/Day6.kt
+++ src/main/kotlin/y2021/Day6.kt
@@ -1,19 +1,35 @@
 package y2021
 
 import lib.Day
-import lib.resourceLines
-import lib.splitByDoubleNewLine
+import lib.resourceString
 
 object Day6 : Day {
 
-    private val input = resourceLines(2021, 6)
+    private val input = resourceString(2021, 6).split(',')
+        .map { it.trim().toInt() }
+        .groupBy { it }
+        .mapValues { it.value.size.toLong() }
 
-    override fun part1() : Long {
-        return -1
-    }
+    override fun part1() = runSimulation(80)
+    override fun part2() = runSimulation(256)
 
-    override fun part2() : Long {
-        return -1
+    private fun runSimulation(daysToRun: Int): Long {
+        var state = input
+        for (day in 0 until daysToRun) {
+            val newState = mapOf(
+                0 to state.getOrDefault(1, 0L),
+                1 to state.getOrDefault(2, 0L),
+                2 to state.getOrDefault(3, 0L),
+                3 to state.getOrDefault(4, 0L),
+                4 to state.getOrDefault(5, 0L),
+                5 to state.getOrDefault(6, 0L),
+                6 to state.getOrDefault(0, 0L) + state.getOrDefault(7, 0L),
+                7 to state.getOrDefault(8, 0L),
+                8 to state.getOrDefault(0, 0L)
+            )
+            state = newState
+        }
+        return state.values.sumOf { it }
     }
 }
 
