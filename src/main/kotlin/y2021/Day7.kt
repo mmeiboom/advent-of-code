b2b4ad0a6ad6c812e2d67c1c8dd319406818638f
Mattijs Meiboom <mmeiboom@bol.com>
2021-12-07 09:44:30 +0100

    Pushed qad version day7

--- src/main/kotlin/y2021/Day7.kt
+++ src/main/kotlin/y2021/Day7.kt
@@ -2,17 +2,36 @@ package y2021
 
 import lib.Day
 import lib.resourceLines
+import lib.resourceString
+import kotlin.math.absoluteValue
 
 object Day7 : Day {
 
-    private val input = resourceLines(2021, 6)
+    private val input = resourceString(2021, 7)
+        .split(',')
+        .map { it.trim().toInt() }
 
     override fun part1() : Long {
-        return -1
+        val min = input.minOrNull()!!
+        val max = input.maxOrNull()!!
+
+        return IntRange(min,max).map { p ->
+            input.sumOf { (it - p).absoluteValue }
+        }.minOrNull()!!.toLong()
     }
 
     override fun part2() : Long {
-        return -1
+        val min = input.minOrNull()!!
+        val max = input.maxOrNull()!!
+
+        return IntRange(min,max).map { p ->
+            input.sumOf { fuelFromTo(it, p) }
+        }.minOrNull()!!.toLong()
+    }
+
+    private fun fuelFromTo(it: Int, p: Int) : Int{
+        val dist = (it - p).absoluteValue
+        return (dist * (dist + 1)) / 2
     }
 }
 
